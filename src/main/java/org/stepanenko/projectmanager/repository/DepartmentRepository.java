fdbd2525a374cc044b3761de024ec6755eb77f77
Eugene Stepanenko <jst1749.7@gmail.com>
2023-02-02 22:57:17 +0500

    add findByName method for DepartmentRepository class and add tests for it

--- src/main/java/org/stepanenko/projectmanager/repository/DepartmentRepository.java
+++ src/main/java/org/stepanenko/projectmanager/repository/DepartmentRepository.java
@@ -1,7 +1,15 @@
 package org.stepanenko.projectmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.stepanenko.projectmanager.model.Client;
 import org.stepanenko.projectmanager.model.Department;
 
+import java.util.Optional;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    @Query("SELECT d FROM Department d WHERE d.name = :name")
+    Optional<Department> findByName(@Param("name") String name);
 }
---
+++ src/test/java/org/stepanenko/projectmanager/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,39 @@
+package org.stepanenko.projectmanager.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.stepanenko.projectmanager.model.Department;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class DepartmentRepositoryTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Test
+    public void givenExistingDepartmentName_whenFindByName_thenReturnDepartment() {
+        String name = "AR";
+        Department expected = new Department(name, List.of());
+
+        departmentRepository.save(expected);
+
+        Department actual = departmentRepository.findByName(name).get();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void givenNotExistingDepartmentName_whenFindByName_thenReturnOptionalEmpty() {
+        String name = "AR";
+
+        Optional<Department> actual = departmentRepository.findByName(name);
+
+        assertThat(actual).isEqualTo(Optional.empty());
+    }
+}
